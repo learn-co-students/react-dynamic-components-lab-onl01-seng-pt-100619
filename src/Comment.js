f08ed317c6e7dc530bc4f399b672b58cb7d40778
Wendy Harris <wendyv510@hotmail.com>
2020-07-23 17:35:18 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-class Comment extends React.Component { 
+class Comment extends Component { 
     render() { 
         return( 
-            <div> {this.props.commentText} </div>
+            <div className='comment' > {this.props.commentText} </div>
         )
     }
 }
